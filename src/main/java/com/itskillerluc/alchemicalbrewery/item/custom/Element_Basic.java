9c93f08f85ff48da43d6f26dd94e3fa5021e0441
Itskillerluc <lvgraef@zeelandnet.nl>
2022-03-11 18:41:44 +0100

    few fixes and small addition

--- src/main/java/com/itskillerluc/alchemicalbrewery/item/custom/Element_Basic.java
+++ src/main/java/com/itskillerluc/alchemicalbrewery/item/custom/Element_Basic.java
@@ -3,9 +3,16 @@ package com.itskillerluc.alchemicalbrewery.item.custom;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 
 public class Element_Basic extends Item {
     public Element_Basic(Properties pProperties) {
@@ -15,6 +22,28 @@ public class Element_Basic extends Item {
     @Override
     public Component getName(ItemStack pStack) {
         return pStack.hasTag() ? new TranslatableComponent(getDescriptionId(), "\u00A7a(" + pStack.getTag().getString("Element") + ")") : new TranslatableComponent("item.alchemicalbrewery.element_basic");
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        Level plevel = pAttacker.getLevel();
+        if(pTarget.isDeadOrDying()){
+            //TODO: add config setting (optional. default is this)
+            if(plevel.getServer().isDedicatedServer()){
+                if (pTarget instanceof Player){
+                    if(pAttacker instanceof Player){
+                        pAttacker.sendMessage(new TextComponent("TEST"), pAttacker.getUUID());
+                    }
+                }
+            }else{
+                if (pTarget instanceof LivingEntity){
+                    if(pAttacker instanceof Player){
+                        pAttacker.sendMessage(new TextComponent("TEST"), pAttacker.getUUID());
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public static class ColorHandler implements ItemColor{
